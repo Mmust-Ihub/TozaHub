f093a9d5870e130ec0d0e2a26fa4401bc29da3c3
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-10-25 19:11:24 +0300

    update the index view to return all available endpoints

--- backend/django/core/urls.py
+++ backend/django/core/urls.py
@@ -1,17 +1,42 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, get_resolver
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 
 
+@permission_classes([AllowAny])
 @api_view(["GET"])
 def index(request):
-    return Response({"message": "Welcome to Toza Hub API"})
+    resolver = get_resolver()
+    urls = []
+
+    def fetch_urls(urlpatterns, base=""):
+        for pattern in urlpatterns:
+            full_path = base + str(pattern.pattern)
+            if full_path.startswith("admin/"):
+
+                continue
+
+            if hasattr(pattern, "url_patterns"):
+                fetch_urls(pattern.url_patterns, full_path)
+
+            else:  # Add individual pattern
+                urls.append(full_path)
+
+    fetch_urls(resolver.url_patterns)
+    # print(urls)
+    return Response(
+        {
+            "message": "Welcome to Toza Hub API",
+            "urls": urls,
+        }
+    )
 
 
 urlpatterns = [
@@ -43,4 +68,4 @@ urlpatterns = [
         name="sacco",
     ),
 ]
-
+# print(urlpatterns)
